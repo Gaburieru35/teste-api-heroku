50f64b96eaf9bf6fc29add203a2182fe1728ec1a
Gabriel Henrique Mota <64985532+Gaburieru35@users.noreply.github.com>
2023-10-22 17:02:01 -0300

    Add files via upload

---
+++ routes/veiculo.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const mysql = require('../mysql').pool
+
+router.get("/", (req, res, next)=>{
+    mysql.getConnection((error, conn) => {
+        if(error){return res.status(500).send({error: error.message, status: 500})}
+        conn.query(
+            "SELECT * FROM veiculo",
+            (error, result, fields) => {
+                conn.release()
+                if(error){return res.status(500).send({error: error}) }
+                else{ return res.status(200).send({cliente: result}) }
+            }
+        )
+    })
+})
+
+router.get("/:id", (req, res, next)=>{
+    const id = req.params.id
+    mysql.getConnection((error, conn) => {
+        if(error){return res.status(500).send({error: error.message, status: 500})}
+        conn.query(
+            `SELECT * FROM veiculo WHERE idVeiculo = ${id}`,
+            (error, result, fields) => {
+                conn.release()
+                if(error){return res.status(500).send({error: error}) }
+                if(result.length > 0){ return res.status(200).send({skill: result}) }
+                else{ return res.status(404).send({error: {statusCode: 404, description: "Not Found"}})}
+            }
+        )
+    })
+})
+
+module.exports = router
