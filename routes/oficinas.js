cc9a3082c70e1ecb1c05191afb4a0388e6a60d50
Gabriel Henrique Mota <64985532+Gaburieru35@users.noreply.github.com>
2023-11-05 18:33:43 -0300

    Add files via upload

--- routes/oficinas.js
+++ routes/oficinas.js
@@ -33,4 +33,19 @@ router.get("/:id", (req, res, next) => {
     })
 })
 
+router.get("pesquisar/:textoPesquisa", (req, res, next) => {
+    const text = req.params.textoPesquisa
+    mysql.getConnection((error, conn) => {
+        if(error){return res.status(500).send({error: error.message, status: 500})}
+        conn.query(
+            `SELECT * FROM oficinas WHERE nome like '${text}'`,
+            (error, result, fields) => {
+                if(error){ return res.status(500).send({error: error}) }
+                if(result.length > 0){ return res.status(200).send(result) }
+                else{ return res.status(404).send({error: {stausCode: 404, description: "Not Found"}}) }
+            }
+        )
+    })
+})
+
 module.exports = router
