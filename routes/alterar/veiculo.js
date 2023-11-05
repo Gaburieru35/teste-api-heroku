07f6524866c8f1a8143935e199a8c877fbcb9e58
Gabriel Henrique Mota <64985532+Gaburieru35@users.noreply.github.com>
2023-11-05 16:37:12 -0300

    Add files via upload

--- routes/alterar/veiculo.js
+++ routes/alterar/veiculo.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const mysql = require('../mysql').pool
+const mysql = require('../../mysql').pool
 
 router.put("/:id", (req, res, next)=>{
     const id = req.params.id
