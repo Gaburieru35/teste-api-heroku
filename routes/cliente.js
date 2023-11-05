2289d4d612ca6cd604a223e4befa61bd916a7227
Gabriel Henrique Mota <64985532+Gaburieru35@users.noreply.github.com>
2023-11-05 14:59:06 -0300

    Add files via upload

--- routes/cliente.js
+++ routes/cliente.js
@@ -6,7 +6,7 @@ router.get("/", (req, res, next)=>{
     mysql.getConnection((error, conn) => {
         if(error){return res.status(500).send({error: error.message, status: 500})}
         conn.query(
-            "SELECT * FROM cliente",
+            "SELECT * FROM cliente c join usuario u on u.idUusario = c.usuario_idUsuario",
             (error, result, fields) => {
                 conn.release()
                 if(error){return res.status(500).send({error: error}) }
@@ -21,7 +21,24 @@ router.get("/:id", (req, res, next)=>{
     mysql.getConnection((error, conn) => {
         if(error){return res.status(500).send({error: error.message, status: 500})}
         conn.query(
-            `SELECT * FROM cliente WHERE usuario_IdUsuario = ${id}`,
+            `SELECT * FROM cliente c join usuario u on u.idUusario = c.usuario_idUsuario WHERE usuario_IdUsuario = ${id}`,
+            (error, result, fields) => {
+                conn.release()
+                if(error){return res.status(500).send({error: error}) }
+                if(result.length > 0){ return res.status(200).send({cliente: result}) }
+                else{ return res.status(404).send({error: {statusCode: 404, description: "Not Found"}})}
+            }
+        )
+    })
+})
+
+router.put("/:id", (req, res, next)=>{
+    const id = req.params.id
+    const { nome } = req.body;
+    mysql.getConnection((error, conn) => {
+        if(error){return res.status(500).send({error: error.message, status: 500})}
+        conn.query(
+            `update usuario set nome = ${nome} WHERE IdUsuario = ${id}`,
             (error, result, fields) => {
                 conn.release()
                 if(error){return res.status(500).send({error: error}) }
