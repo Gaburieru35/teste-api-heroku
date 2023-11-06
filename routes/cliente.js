548ac558bc49daa591a97035aec5147ad8dddbff
Matheus Vinicius Mota <64799886+MatVini0601@users.noreply.github.com>
2023-11-06 16:36:23 -0300

    Update cliente.js

--- routes/cliente.js
+++ routes/cliente.js
@@ -16,6 +16,35 @@ router.get("/", (req, res, next)=>{
     })
 })
 
+router.post("/", (req, res, next)=>{
+    mysql.getConnection((error, conn) => {
+        const id = Date.now();
+        const cliente = req.body 
+        if(error){return res.status(500).send({error: error.message, status: 500})}
+        const sql1 = "INSERT INTO usuario (`idUsuario`, `nomeUsuario`, `cpfUsuario`) VALUES ?"
+        let values1 = [[id, cliente.nome, cliente.cpf]]
+       
+        conn.query(sql1, [values1],
+            (error, result, fields) => {
+                console.log(error)
+                // if(error){return res.status(500).send({error: error}) }
+                // else{ return res.status(200).send({cliente: result}) }
+            }
+        )
+
+        const sql2 = "INSERT INTO cliente (`emailCliente`, `senhaCliente`, `endereçoCliente`, `telefoneCliente`, `usuario_idUsuario`) VALUES ?"
+        let values2 = [[cliente.email, cliente.senha, cliente.endereco, cliente.telefone, id]]
+
+        conn.query(sql2, [values2],
+            (error, result, fields) => {
+                conn.release()
+                if(error){return res.status(500).send({error: error}) }
+                else{ return res.status(200).send({cliente: result}) }
+            }
+        )
+    })
+})
+
 router.get("/:id", (req, res, next)=>{
     const id = req.params.id
     mysql.getConnection((error, conn) => {
@@ -27,6 +56,40 @@ router.get("/:id", (req, res, next)=>{
                 if(error){return res.status(500).send({error: error}) }
                 if(result.length > 0){ return res.status(200).send({cliente: result}) }
                 else{ return res.status(404).send({error: {statusCode: 404, description: "Not Found"}})}
+            }
+        )
+    })
+})
+
+router.put("/alterar/:id", (req, res, next)=>{
+    const id = req.params.id
+    const cliente = req.body 
+
+    const sql1 = `UPDATE usuario SET
+            nomeUsuario = "${cliente.nome}",
+            cpfUsuario = "${cliente.cpf}"
+            where idUsuario = ${id}`
+
+    const sql2 =  `UPDATE cliente SET
+                 emailCliente = "${cliente.email}",
+                 senhaCliente = "${cliente.senha}",
+                 endereçoCliente = "${cliente.endereco}",
+                 telefoneCliente = "${cliente.telefone}"
+                 where usuario_idUsuario = ${id}`
+
+
+    mysql.getConnection((error, conn) => {
+        if(error){return res.status(500).send({error: error.message, status: 500})}
+        conn.query(sql1,
+            (error, result, fields) => {
+                console.log(error)
+            }
+        )
+
+        conn.query(sql2,
+            (error, result, fields) => {
+                if(error){return res.status(500).send({error: error}) }
+                if(result.length > 0){ return res.status(200).send({cliente: result}) }
             }
         )
     })
