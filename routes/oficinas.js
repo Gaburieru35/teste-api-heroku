f48f393dce7a4a731c1924e3bd97107c7390df6b
Gabriel Henrique Mota <64985532+Gaburieru35@users.noreply.github.com>
2023-11-11 17:20:17 -0300

    Add files via upload

--- routes/oficinas.js
+++ routes/oficinas.js
@@ -21,14 +21,25 @@ router.get("/pesquisar", (req, res, next) => {
     const textoPesquisa = req.query.textoPesquisa;
     mysql.getConnection((error, conn) => {
         if(error){return res.status(500).send({error: error.message, status: 500})}
-        conn.query(
-            `SELECT * FROM oficinas WHERE nome like '%${textoPesquisa}%'`,
-            (error, result, fields) => {
-                if(error){ return res.status(500).send({error: error}) }
-                if(result.length > 0){ return res.status(200).send(result) }
-                else{ return res.status(404).send({error: {stausCode: 404, description: "Not Found"}}) }
-            }
-        )
+        if(textoPesquisa == ""){
+            conn.query(
+                `SELECT * FROM oficinas`,
+                (error, result, fields) => {
+                    if(error){ return res.status(500).send({error: error}) }
+                    if(result.length > 0){ return res.status(200).send(result) }
+                    else{ return res.status(404).send({error: {stausCode: 404, description: "Not Found"}}) }
+                }
+            )
+        } else{
+            conn.query(
+                `SELECT * FROM oficinas WHERE nome like '%${textoPesquisa}%'`,
+                (error, result, fields) => {
+                    if(error){ return res.status(500).send({error: error}) }
+                    if(result.length > 0){ return res.status(200).send(result) }
+                    else{ return res.status(404).send({error: {stausCode: 404, description: "Not Found"}}) }
+                }
+            )
+        }
     })
 })
 
